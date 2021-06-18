e1f56c0cf927e3dc32edaedcc45ea0848415cbf4
Santosh Bhattarai <hsotnas76@gmail.com>
2021-06-19 00:45:56 +0545

    initial commit

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:bloc_tutorial/counter_event.dart';
+
+class CounterBloc {
+  int _counter = 0;
+
+  StreamController<CounterEvent> _counterEventController =
+      StreamController<CounterEvent>();
+
+  Sink<CounterEvent> get eventSink => _counterEventController.sink;
+
+  StreamController<int> _counterStateController = StreamController<int>();
+
+  Sink<int> get _incounter => _counterStateController.sink;
+
+  Stream<int> get outputStream => _counterStateController.stream;
+
+  CounterBloc() {
+    _counterEventController.stream.listen(_mapEventToState);
+  }
+
+  _mapEventToState(CounterEvent event) {
+    if (event is IncrementEvent) {
+      _counter++;
+    }
+    if (event is DecrementEvent) {
+      _counter--;
+    }
+    _incounter.add(_counter);
+  }
+
+  void dispose() {
+    _counterStateController.close();
+    _counterEventController.close();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:bloc_tutorial/counter_event.dart';
+import 'package:flutter/material.dart';
+
+import 'counter_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  final _bloc = CounterBloc();
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    print("build called");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder(
+                stream: _bloc.outputStream,
+                builder: (context, snapshot) {
+                  return Text(
+                    '$snapshot',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => _bloc.eventSink.add(IncrementEvent()),
+            tooltip: '',
+            child: Icon(Icons.add),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () => _bloc.eventSink.add(DecrementEvent()),
+            tooltip: 'Decrement',
+            child: Icon(Icons.remove),
+          )
+        ],
+      ),
+    );
+  }
+}
